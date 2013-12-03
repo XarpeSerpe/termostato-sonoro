50ec8933cdb3e789c10261058008cda41b65b006
XarpeSerpe <xarpeserpe@gmail.com>
2013-12-03 18:55:06 +0100

    Update termosono.ino

--- termosono.ino
+++ termosono.ino
@@ -1,93 +1,258 @@
 /*
-Usar un Arduino Uno para controlar la temperatura de una habitacion
-La media de temperatura se realiza con una termoresistencia que debe ser calibrada
-La temperatura deseada se selecciona con dos botones que la subiran o bajaran a tramos de medio grado
-La temperatura deseada se anuncia bien por pitidos (posteriores versione slo haran con mp3)
-La subida se anuncia con tonos agudos, un pitido por cada tramo que se separe de 20 ºC
-La bajada se anuncia con tonos graves, un pitido por cada tramos que se separe de 20 ºC
-La temperatura por defecto es 20.0 ºC
+Se usa un Arduino Uno R3 para controlar la temperatura de una habitacion.
+Se usa una VS1053 MP3 Shield de Geeetech para informar verbalmente de la temperatura seleccionada.
+La media de temperatura se realiza con un divisor de tension hecho una termoresistencia y una resistencia.
+La termoresistencia debe ser calibrada.
+La temperatura por defecto es 20.0 ºC.
+La temperatura deseada se selecciona con dos sensores capacitivos que la subiran o bajaran a tramos de medio grado.
+La temperatura deseada se anuncia con la reproduccion del fichero mp3 adecuado.
+Evita conexiones y desconexiones rapidas del rele espaciandolas 30 segundos.
+Para el acceso al fichero mp3 se usa la libreria  SdFat y SdFatUtil desarrolladas por XXX.
+Para reproducir el fichero mp3 se usa la libreria SFEMP3Shield desarrollada por Bill Porter y Michael P. Flaga.
+ */
 
-Hay que eliminar los "rebotes" de las interrupciones, bien por hardware bien por software.
-Por software es haciendo que no se active la interrupcion si no es superior a un tiempo dado
+#include <SPI.h>
+#include <SdFat.h>
+#include <SdFatUtil.h>
+#include <SFEMP3Shield.h>
+/* Below is not needed if interrupt driven. Safe to remove if not using.
+#if defined(USE_MP3_REFILL_MEANS) && USE_MP3_REFILL_MEANS == USE_MP3_Timer1
+  #include <TimerOne.h>
+#elif defined(USE_MP3_REFILL_MEANS) && USE_MP3_REFILL_MEANS == USE_MP3_SimpleTimer
+  #include <SimpleTimer.h>
+#endif*/
+#include <CapacitiveSensor.h>
+
+//Inicializa la tarjeta SD
+SdFat sd;
+//Inicializa la Shield MP3
+SFEMP3Shield MP3player;
+
+CapacitiveSensor sube = CapacitiveSensor(2,4);
+CapacitiveSensor baja = CapacitiveSensor(2,0);
+
+const int sensor = A0;// lee el la caida de voltaje en la termoresistencia.
+const int rele   = 19;// A5 control del rele que activa o desactiva la caldera.
+/*
+Cambiar por interrupciones.
+En la Shield MP3 el pin 2 controla el DREG, la derivo al pin 0 de Arduino.
+Esto precisa cambiar el uso del pin 2 de Arduino al pin 0 de Arduino bien en la libreria SDFAT bien en la de MP3, estudiar esto
+const int sensor_subir = 3;//Derivacion de la interrupcion para subida de temperatura.
+const int sensor_bajar = 2;//Derivacion de la interrupcion para bajada de temperatura.
+
 */
-//pins interrupt pins 2, 3
-
-const int medir_1   = 7;
-const int termo_1   = A0;
-const int speaker   = 11;
-const int rele      = 8;
-
-int i; // para el contador dentro de las interrupciones
+int temp_v; //la "temperatura" medida por el lm35 en el A0
 float temp; //temperatura actual
-volatile int filtro = 2000; //Tiempo humbral para activar una interrupcion, testa tiempo idoneo ya que el rebote depende del interruptor usado
-// Ojo al uso de millis(), a los 70 dias se reinicia, hay que capturar esta excepcion
-volatile int lastInt = 0;
-
-volatile float temp_obj = 250.0; //temperatura objetivo aun sin mapear
-float orquilla = 0.25;
+volatile int temp_obj = 21; //temperatura objetivo aun sin mapear
+float orquilla = 5; //temperatura objetivo aun sin mapear
 
 void setup()
-  {
-    Serial.begin(9600);
-    
-    pinMode(medir_1, OUTPUT);
-    pinMode(termo_1, INPUT);
-    pinMode(speaker, OUTPUT);
-    pinMode(rele,    OUTPUT);
-    pinMode(2,       INPUT);
-    pinMode(3,       INPUT);
-    digitalWrite(2, HIGH); //pull-up Esto elimina los rebotes?
-    digitalWrite(3, HIGH); //pull-up
-    
-    digitalWrite(medir_1, LOW);
-    digitalWrite(speaker, LOW);
-    digitalWrite(rele, LOW);
-    
-    attachInterrupt(0, aumentar_temp, RISING);
-    attachInterrupt(1, disminuir_temp, RISING);
-  }
+{
+  Serial.begin(115200);
+  //Initialize the SdCard.
+  if(!sd.begin(SD_SEL, SPI_HALF_SPEED)) sd.initErrorHalt();
+  if(!sd.chdir("/")) sd.errorHalt("sd.chdir");
+  //Initialize the MP3 Player Shield
+  
+  pinMode(sensor, INPUT);
+  pinMode(rele,   OUTPUT);
+  
+  digitalWrite(rele, LOW);
+  
+  MP3player.begin();
+  suena("inicio.mp3");
+  delay(2000); // Hay que dar tiempo a termine la reproduccion.
+  
+}
 
-void medir() // Compara temperatura actual con la temperatura deseada. Posiblemente se pueda mejorar para no repetir estados.
-  {
-    digitalWrite(medir_1, HIGH);
-    temp = analogRead(termo_1); // esta funcion lee el voltaje, no la intensidad. Por esto se precisa que la termistor este en un divisor de corriente
-    Serial.print(temp);
-    Serial.print("   ");
-    Serial.println(temp_obj);
-    
-    
-    if(temp < temp_obj)
-      {
-        digitalWrite(rele, HIGH);
-      }
-    else
-      {
-        digitalWrite(rele, LOW);
-      }
-    
-  }
-  
-void aumentar_temp()//interrupcion
-  {
-    
-      temp_obj = temp_obj + 5; // esta sin mapear
-      for(i=0 ; i < 2000; i++)
-      tone(speaker, 40, 1000);
-      
-      
-  }
-  
-void disminuir_temp() //interrupcion
-  {
-    
-      temp_obj = temp_obj - 5; //esta sin mapear
-      for(i=0 ; i < 2000; i++)
-      tone(speaker, 2000, 1000);
-     
-  }
-  
+
 void loop()
+{
+  medir_temp();  
+  sensor_capacitivo();  
+}
+
+void medir_temp() // Compara temperatura actual con la temperatura deseada. Posiblemente se pueda mejorar para no repetir estados.
+{// esta funcion lee el voltaje, no la intensidad. Por esto se precisa que la termistor este en un divisor de corriente
+  
+  temp_v = analogRead(sensor);
+  temp = 20.0 / 500.0 * temp_v; //recalibrar esta transformacion, usar regresion lineal
+  Serial.print(temp);
+  Serial.print("   ");
+  Serial.println(temp_obj);
+  if(temp < temp_obj)
   {
-    medir();
-    delay(1000);
+    digitalWrite(rele, HIGH);
   }
+  else
+  {
+    digitalWrite(rele, LOW);
+  }
+}
+
+void suena(char trackName[])
+{
+  MP3player.playMP3(trackName, 0);
+}
+
+void sensor_capacitivo() //interrupcion
+{
+  long tiempo_sube;
+  long tiempo_baja;
+    
+  tiempo_sube = sube.capacitiveSensor(30);
+  tiempo_baja = baja.capacitiveSensor(30);
+  
+  if(tiempo_sube > 500 && tiempo_baja < 500) // estos valores deben ser testados
+  {
+    temp_obj = temp_obj + 5; // esta sin mapear
+    if (temp_obj > 255) temp_obj = 250;
+
+    switch(temp_obj)
+    {
+      case 150:
+        suena("150.mp3");  
+      break;
+      case 155:
+        suena("155.mp3" );  
+      break;
+      case 160:
+        suena("160.mp3" );  
+      break;
+      case 165:
+        suena("165.mp3" );  
+      break;
+      case 170:
+        suena("170.mp3" );  
+      break;
+      case 175:
+        suena("175.mp3" );  
+      break;
+      case 180:
+        suena("180.mp3" );  
+      break;
+      case 185:
+        suena("185.mp3" );  
+      break;
+      case 190:
+        suena("190.mp3" );  
+      break;
+      case 195:
+        suena("195.mp3" );  
+      break;
+      case 200:
+        suena("200.mp3" );  
+      break;
+      case 205:
+        suena("205.mp3" );  
+      break;
+      case 210:
+        suena("210.mp3" );  
+      break;
+      case 215:
+        suena("215.mp3" );  
+      break;
+      case 220:
+        suena("220.mp3" );  
+      break;
+      case 225:
+        suena("225.mp3" );  
+      break;
+      case 230:
+        suena("230.mp3" );  
+      break;
+      case 235:
+        suena("235.mp3" );  
+      break;
+      case 240:
+        suena("240.mp3" );  
+      break;
+      case 245:
+        suena("245.mp3" );  
+      break;
+      case 250:
+        suena("250.mp3" );  
+      break;
+    }
+  }
+  else if(tiempo_sube < 500 && tiempo_baja > 500)
+  {
+    temp_obj = temp_obj - 5; //esta sin mapear
+    if (temp_obj < 150) temp_obj = 150;
+    switch(temp_obj)
+    {
+      case 150:
+        suena("150.mp3" );  
+      break;
+      case 155:
+        suena("155.mp3" );  
+      break;
+      case 160:
+        suena("160.mp3" );  
+      break;
+      case 165:
+        suena("165.mp3" );  
+      break;
+      case 170:
+        suena("170.mp3" );  
+      break;
+      case 175:
+        suena("175.mp3" );  
+      break;
+      case 180:
+        suena("180.mp3" );  
+      break;
+      case 185:
+        suena("185.mp3" );  
+      break;
+      case 190:
+        suena("190.mp3" );  
+      break;
+      case 195:
+        suena("195.mp3" );  
+      break;
+      case 200:
+        suena("200.mp3" );  
+      break;
+      case 205:
+        suena("205.mp3" );  
+      break;
+      case 210:
+        suena("210.mp3" );  
+      break;
+      case 215:
+        suena("215.mp3" );  
+      break;
+      case 220:
+        suena("220.mp3" );  
+      break;
+      case 225:
+        suena("225.mp3" );  
+      break;
+      case 230:
+        suena("230.mp3" );  
+      break;
+      case 235:
+        suena("235.mp3" );  
+      break;
+      case 240:
+        suena("240.mp3" );  
+      break;
+      case 245:
+        suena("245.mp3" );  
+      break;
+      case 250:
+        suena("250.mp3" );  
+      break;
+    }
+  }
+  else if(tiempo_sube > 500 && tiempo_baja > 500)
+  {
+    suena("error.mp3");
+  }
+  else
+  {
+    delay(1);
+  }
+}
+  
