7229084cdb2ad0a35ca8f354ededcfc96ac3928c
XarpeSerpe <xarpeserpe@gmail.com>
2013-12-16 19:01:00 +0100

    Update termoson_con_interrupciones.ino

--- termoson_con_interrupciones.ino
+++ termoson_con_interrupciones.ino
@@ -1,13 +1,13 @@
 /*
 Se usa un Arduino Uno R3 para controlar la temperatura de una habitacion.
 Se usa una VS1053 MP3 Shield de Geeetech para informar verbalmente de la temperatura seleccionada.
-La media de temperatura se realiza con un divisor de tension hecho una termoresistencia y una resistencia.
-La termoresistencia debe ser calibrada.
+Un LM35 mide la temperatura ambiente.
+El LM35 debe ser calibrado.
 La temperatura por defecto es 21.0 ºC.
 La temperatura deseada se selecciona con dos sensores capacitivos que la subiran o bajaran a tramos de medio grado.
 La temperatura deseada se anuncia con la reproduccion del fichero mp3 adecuado.
 Evita conexiones y desconexiones rapidas del rele espaciandolas 30 segundos.
-Para el acceso al fichero mp3 se usa la libreria  SdFat y SdFatUtil desarrolladas por XXX.
+Para el acceso al fichero mp3 se usa la libreria  SdFat y SdFatUtil desarrolladas por William Greiman
 Para reproducir el fichero mp3 se usa la libreria SFEMP3Shield desarrollada por Bill Porter y Michael P. Flaga.
  */
 
@@ -64,7 +64,7 @@ void setup()
   MP3player.begin();
   suena("inicio.mp3");
   delay(2000); // Hay que dar tiempo a termine la reproduccion.
-  //attachInterrupt(0,sube,RISING); //La interrupcion 0 esta en el pin 2
+  attachInterrupt(0,sube,RISING); //La interrupcion 0 esta en el pin 2
   attachInterrupt(1,baja,RISING); //La interrupcion 1 esta en el pin 3
 }
 
