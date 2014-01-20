db9bf2569d46b34b7a4b69bd1eba222f00346aad
XarpeSerpe <xarpeserpe@gmail.com>
2014-01-20 21:03:29 +0100

    Update termoson.ino

--- termoson.ino
+++ termoson.ino
@@ -2,15 +2,16 @@
 Se usa un Arduino Uno R3 para controlar la temperatura de una habitacion.
 Se usa una VS1053 MP3 Shield de Geeetech para informar verbalmente de la temperatura seleccionada.
 Un LM35 mide la temperatura ambiente.
-El LM35 debe ser calibrado.
-La temperatura por defecto es 5.0 ºC.
+La temperatura objetivo por defecto es 5.0 ºC.
 La temperatura deseada se selecciona con un potenciometro.
 La temperatura deseada se anuncia con la reproduccion del fichero mp3 adecuado.
-Se usa la interrupcion 2 (pin 3) para consulta de temperatura.
 Evita conexiones y desconexiones rapidas del rele espaciandolas 30 segundos.
 Para el acceso al fichero mp3 se usa la libreria  SdFat y SdFatUtil desarrolladas por William Greiman
 Para reproducir el fichero mp3 se usa la libreria SFEMP3Shield desarrollada por Bill Porter y Michael P. Flaga.
  */
+/*To Do
+Botón de consulta de temperatura
+*/
 
 #include <SPI.h>
 #include <SdFat.h>
@@ -24,21 +25,20 @@ Para reproducir el fichero mp3 se usa la libreria SFEMP3Shield desarrollada por 
   #include <SimpleTimer.h>
 #endif*/
 
-//Inicializa la tarjeta SD
+//Inicializa el acceso a tarjeta SD
 SdFat sd;
-//Inicializa la Shield MP3
+//Inicializa el acceso a Shield MP3
 SFEMP3Shield MP3player;
 
 const int sensor = A0;// lee el la caida de voltaje en el lm35
 const int rele   = 10;// control del rele que activa o desactiva la caldera.
 const int potenciometro = A5; // regulador de temperatura.
 const int boton = 3; //Si el boton esta pulsado entonces informa de la temperatura actual.
-const int alimentar_potenciometro = 4;
+/* problemas de alimentación del potenciometro, cuando cae la resistencia, cae el potencial del LM35*/
 
 int temp_v; //la "temperatura" medida por el lm35 en el A0
 int temp; //temperatura actual
 int temp_obj = 5.0; // Temperatura objetivo al iniciar el sistema
-int temp_mp3;
 int pot_old = 0;
 int pot_now;
 unsigned long time;
@@ -46,7 +46,7 @@ unsigned long time_old = 0;
 
 void setup()
 {
-  	//attachInterrupt(1, info, RISING);
+  	attachInterrupt(1, info, RISING);
   	Serial.begin(9600);//debug
 
   	//Initialize the SdCard.
@@ -57,10 +57,9 @@ void setup()
   	pinMode(sensor, INPUT);
   	pinMode(rele,   OUTPUT);
   	pinMode(boton, INPUT);
-  	pinMode(alimentar_potenciometro, OUTPUT);
-        pinMode(potenciometro, INPUT);
+  	pinMode(potenciometro, INPUT);
         digitalWrite(rele, LOW);
-	digitalWrite(alimentar_potenciometro, HIGH);
+
        
     		
     //Initialize the MP3 Player Shield
@@ -120,8 +119,8 @@ int seleccion()//ahora puede cambiar sin avisar
   
   if (pot_old != pot_now)//ojo a la inducción del termostato, medir cuando se apaga e impedir que se ejecute el seleccion
   {
-    Serial.print(" Temperatura objetivo ");
-    Serial.print(pot_now);
+    Serial.print("Temperatura objetivo ");
+    Serial.println(pot_now);
     name = String(pot_now);
     name += ".mp3";
     strcpy(fichero, name.c_str());
